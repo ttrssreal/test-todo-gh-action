bcf569b793f75482778c84ec87b706f996e0f723
Jess <jess@jessie.cafe>
2024-08-05 05:40:36 +1200

    Laptop: Make podman not fucky

--- modules/podman.nix
+++ modules/podman.nix
@@ -1,12 +1,14 @@
 { pkgs, ... }: {
   # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
   virtualisation = {
+    docker.enable = true;
+    containers.enable = true;
+
     podman = {
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+      # dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
